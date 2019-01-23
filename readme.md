eecf7492a799fd8566f8aaabc0b9e94d14061e50
Beatriz Pereira Do Valle Staub <beatrizpereiradovallestaub@Beatrizs-MacBook.local>
2019-01-23 17:33:24 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+This is my first sketch for the Interactive Graphics Class. 
+I do not have personal reasons behind why I chose to depict The Simpsons; I searched Google for a donut because it is one of my favorite American desserts and I find the colors appealing. I found a picture of Bart when searching for a pink donut and found that the image could use fun interactions. So then I started playing around with my code and this is what I got.
+I hope you guys all enjoy it :)
+
---
+++ sketch.js
@@ -0,0 +1,28 @@
+function setup (){
+    createCanvas (1200, 600);
+    background(255, 155, 246);
+    img = loadImage('pinkdonut.png');
+    img2 = loadImage('homersimpson.png');
+    img3 = loadImage('pacifier.png')
+    img4 = loadImage('maggiesimpson.png')
+}
+
+
+function draw (){
+    image(img2, 20, 80, 400, 520);
+    image(img4, 980, 300, 210, 325);
+
+    (img, 50, 50, img.width/3, img.height/3);
+    (img2, 100, 100, img.width/5, img.height/5);
+    (img3, 100, 100, img.width/5, img.height/5);
+    (img4, 100, 100, img.width/4, img.height/4);
+
+    if (mouseIsPressed) {
+        image(img3, mouseX, mouseY, 80, 120);
+        
+        
+    }
+    else {
+        image(img, mouseX, mouseY,100, 110);
+    }
+}
